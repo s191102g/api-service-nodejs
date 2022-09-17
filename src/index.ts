3a13af8c8a68a74258cfae8671ab3202e771954f
sang2xx2 <s191102g@gmail.com>
2022-09-17 21:59:17 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import { dev } from "./config/Configuration";
 import { dev2 } from "./config/DbConfig";
+import express from "express"
 
-if(process.env.NODE_ENV == 'production'){
-    console.log(dev2);
-    
-}else{
-    console.log(dev);
-    
-}
+const app = express(); 
+const port = 8080; 
+
+app.get('/', function(req:any, res:any){
+     var en:any=null
+     if (process.env.NODE_ENV=='production') {
+        en = dev2
+     }else{
+        en = dev
+     }
+
+     res.json({
+        data:en
+     })
+})
+
+app.listen(process.env.PORT || port, function(){
+    console.log("Your app running on port ");
+})
+
+
 
 
 
