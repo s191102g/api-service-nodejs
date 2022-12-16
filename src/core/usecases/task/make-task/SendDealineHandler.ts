e5efdc718b28114b09c3849f1f6355aa94d764e7
sang2xx2 <s191102g@gmail.com>
2022-12-16 14:43:29 +0700

    update sendDeadline

--- src/core/usecases/task/make-task/SendDealineHandler.ts
+++ src/core/usecases/task/make-task/SendDealineHandler.ts
@@ -11,14 +11,34 @@ export class SendDealineHandler {
     const _mailService = Container.get<IMailService>('mail.service');
     const _clientRepository = Container.get<IClientRepository>('client.repository');
     const interval = setInterval(async ()=>{
+      // handler dealine
+        const arr1 = param.dealine.split(" ");
+        const arr1b = arr1.slice(0,5);
+        const finalDeadline = arr1b.join(" ")
+          
+        // handler date
         const dateNow = new Date();
         const date = dateNow.toString();
-        const dealine = param.dealine;
+        const arr2 = date.split(" ");
+        const arr2b = arr2.slice(0,5);       
+        const arrtime = arr2b[4].split(":")
+        const hh = Number(arrtime[0]) + 7;
+         arrtime.splice(0,1,String(hh));
+        const fnTime = arrtime.join(":")
+         arr2b.splice(4,1,fnTime)
+
+         const finalDate = arr2b.join(" ");
+
+         console.log(
+          finalDeadline,
+          finalDate
+         );
+         
         
-        if(date === dealine){ 
+        if(finalDate === finalDeadline){ 
             clearItv();
         }
-        console.log(date);
+        // console.log(date);
         
          
      },1000);
